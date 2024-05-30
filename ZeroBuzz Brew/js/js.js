a97afafe4818f91b4a0292321a518c4a4ed57a97
LaerkeSkjerning <161304822+LaerkeSkjerning@users.noreply.github.com>
2024-05-30 14:55:00 +0200

    Delete ZeroBuzz Brew directory

--- ZeroBuzz Brew/js/js.js
+++
@@ -1,6 +0,0 @@
-const mobileMenu = document.getElementById('mobile-menu');
-const navUl = document.querySelector('nav ul');
-
-mobileMenu.addEventListener('click', () => {
-  navUl.classList.toggle('showing');
-});
